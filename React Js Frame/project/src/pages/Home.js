1ba910972c7d6c14a51b6408ef43aedde506a2fa
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-22 01:02:25 +0530

    Added react router dom

--- React Js Frame/project/src/pages/Home.js
+++ React Js Frame/project/src/pages/Home.js
@@ -6,16 +6,6 @@ export default function Home() {
   return (
     <div>
         <h1>Home page</h1>
-        <TopicBox food = "Koththu" price = "Rs 250" >
-            <span>This is the description</span>
-        </TopicBox>
-        <TopicBox food = "Hoppers" price = "Rs 350" >
-            <button>Click me!</button>
-        </TopicBox>
-        <TopicBox food = "Rice" price = "Rs 150" >
-            <p>This is paragraph</p>
-        </TopicBox>
-        <Content></Content>
     </div>
   )
 }
