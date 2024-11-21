664eb6bb8372c24559562897f7711b9cb0a98757
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-22 02:06:35 +0530

    Day 1 finalized

--- React Js Frame/project/src/App.js
+++ React Js Frame/project/src/App.js
@@ -1,9 +1,22 @@
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import TopicBox from './components/TopicBox';
+import Content from './components/Content';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Router>
+      <div>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
--- React Js Frame/project/src/pages/Home.js
+++ React Js Frame/project/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopicBox from '../components/TopicBox';
 import Content from '../components/Content';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div>
-        <h1>Home page</h1>
+        <h1>This is home</h1>
+        <link to="/About">Go to about page</link>
     </div>
-  )
+  );
 }
