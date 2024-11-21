cc3719994ef447244cb6d90fd112ec826d2ea461
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-22 00:55:15 +0530

    Work with js props

--- React Js Frame/project/src/components/TopicBox.js
+++ React Js Frame/project/src/components/TopicBox.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './TopicBox.css';
 
-export default function TopicBox() {
-  return (
+export default function TopicBox(props) {
+    const topic = 'Topic bar';
+    return (
     <div>
         <div className="topicBox">
-            <span className='text'>This is the topic</span>
+            <span className='text'>{props.food} price is: {props.price}</span>
+            {props.children}
         </div>
     </div>
   )
