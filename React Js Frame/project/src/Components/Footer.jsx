25e6a054c5afacc44baf06157d824dd90fbd2122
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-22 10:52:46 +0530

    Button update

--- React Js Frame/project/src/Components/Footer.jsx
+++ React Js Frame/project/src/Components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Footer() {
-  return (
-    <div>
-        <Link to="/">Go to Home</Link><br />
-        <Link to="/About">Go to About</Link><br />
-        <Link to="/Profile">Go to profile</Link>
-    </div>
-  )
+    const navigate = useNavigate();
+    return (
+        <div>
+            <button></button>
+            <button onClick={() => navigate("/profile")}>
+                Profile
+            </button>
+        </div>
+    );
 }
